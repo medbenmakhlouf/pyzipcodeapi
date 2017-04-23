5472ea4e87e2aa53cfbd3f4156ffb0526ba994cf
Mohamed Ben Makhlouf <benmakhlouf@telehealthmgt.com>
2017-04-23 10:49:41 +0100

    Setup projects

---
+++ pyzipcodeapi/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__title__ = 'pyzipcodeapi'
+__version__ = '1.0.0'
+__build__ = 0x022200
+__author__ = 'Mohamed Ben Makhlouf'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2017 Mohamed Ben Makhlouf'
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '1.0.0'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='pyzipcodeapi',
+    version=__version__,
+    description='here description that I should change',
+    long_description=long_description,
+    url='https://github.com/medbenmakhlouf/pyzipcodeapi',
+    download_url='https://github.com/medbenmakhlouf/pyzipcodeapi/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Mohamed Ben Makhlouf',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='med.b.makhlouf@gmail.com'
+)
